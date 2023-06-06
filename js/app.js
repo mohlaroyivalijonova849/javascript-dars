f820d8151f78149576226ba83267dbf74e770e1d
mohlaroyivalijonova849 <mokhlaroyim.valijonova@gmail.com>
2023-06-06 17:11:27 +0500

    mening birinchi komentim

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// let name="Mohlaroyim";
+// let firstName="Valijonova";
+// const from="Uzbekistan";
+// let village="Oltiariq";
+// let age=17;
+// let year=2006;
+// document.write("Valijonova Mohlaroyim")
+// let ism=annsc
+// let firstName=Valijonova
+// console.log(`SALOM, MENING ISMIM ${ism} ${firstName}`)
